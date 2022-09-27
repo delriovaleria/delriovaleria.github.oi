fbe1671d3a24ab52aa064a8e8e1979fe8ba1bd56
delriovaleria <112910155+delriovaleria@users.noreply.github.com>
2022-09-27 18:16:34 -0500

    Add files via upload

---
+++ piano/04-dom.js
@@ -0,0 +1,3 @@
+let name= document.querySelector("#name");
+let age= document.querySelector("#age");
+let likeMusic= document. querySelector("#likesMusic");
---
+++ piano/jsmain.js
@@ -0,0 +1,2 @@
+const white_Keys=['z','x','c','v','b','n','m'];
+const black_Keys=['s','d','g','h','j'];
